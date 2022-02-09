b56736104c83aaefc8cfe19b1ff2a0d952e9f983
Ommote <olivia.motevalli@gmail.com>
2022-02-09 20:28:45 +0000

    built and styled nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import Main from "./components/Main/Main"
 import beers from './data/beer';
+import Nav from './components/Nav/Nav'
 
 console.log(beers)
 
 function App() {
   return (
     <div className="App">
+      <Nav />
       <Main 
       beersArr={beers}
       />
--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,7 +3,18 @@ import "./Nav.scss"
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <div className='NavBar'>
+      <label htmlFor="search" for="search" ></label>
+      <input type="search" name='search' id='search' placeholder='Search'/>
+      <br />
+      <label htmlFor="abv" for="abv">High ABV (&gth;6.0%)</label>
+      <input className="check" type="checkbox" name='abv' id='abv' dir="rtl"/>
+      <label htmlFor="classic" for="classic">Classic Range</label>
+      <input className="check" type="checkbox" name="classic" id="classic" dir="rtl"/>
+      <label htmlFor="acidic" for="acidic">Acidic (ph &lth; 4)</label>
+      <input className="check" type="checkbox" name="acidic" id="acidic" dir="rtl"/>
+
+    </div>
   )
 }
 
